6b7e0d1ba60853d2458fdb6e62a7b78b974d68fd
Purwo Handoyo <handoyopurwo600@gmail.com>
2023-11-23 11:01:44 +0700

    fix bug

--- src/DockerService.jsx
+++ src/DockerService.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 
 const DockerService = () => {
     const [status, setStatus] = useState('');
-    const [logs, setLogs] = useState([]);
     const [dockerInfo, setDockerInfo] = useState({
         memTotal: '?',
         NGoroutines: '?',
