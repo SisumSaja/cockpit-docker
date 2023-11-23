7f8a37e45027850acbbf0ebe1de067dc4652188c
Purwo Handoyo <handoyopurwo600@gmail.com>
2023-11-23 10:45:07 +0700

    first init

---
+++ src/DockerService.jsx
@@ -0,0 +1,102 @@
+import React, {useEffect, useState} from 'react';
+import {Terminal} from "./Terminal";
+
+const DockerService = () => {
+    const [status, setStatus] = useState('');
+    const [logs, setLogs] = useState([]);
+    const [dockerInfo, setDockerInfo] = useState({
+        memTotal: '?',
+        NGoroutines: '?',
+        NFd: '?',
+        Containers: '?',
+        Images: '?',
+    })
+
+    const addToLogs = (message, type = 'info') => {
+        setLogs((prevLogs) => [...prevLogs, {message, type}]);
+    };
+    const installDocker = async () => {
+        addToLogs('Installing Docker...', 'info');
+        try {
+            const response = await fetch('http://localhost:5000/install-docker', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                addToLogs(data.message, 'success');
+            } else {
+                addToLogs(`Error: ${data.error}`, 'error');
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+            addToLogs('An error occurred.', 'error');
+        }
+    };
+    const buildDocker = () => {
+        setStatus('Building Docker Image...');
+        // Simulate the build process (replace with actual build commands)
+        setTimeout(() => {
+            setStatus('Docker Image built successfully!');
+        }, 3000);
+    };
+    const dockerCompose = () => {
+        setStatus('Running Docker Compose...');
+        // Simulate the docker-compose process (replace with actual compose commands)
+        setTimeout(() => {
+            setStatus('Docker Compose completed successfully!');
+        }, 3000);
+    };
+
+
+    async function retrieveInfo() {
+        try {
+            const response = await fetch('/var/run/docker.sock/info', {method: 'GET'})
+            const data = await response.json();
+            setDockerInfo(data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    useEffect(() => {
+        // Initial retrieval
+        retrieveInfo();
+
+        // Event listener for updates
+        const eventSource = new EventSource('/var/run/docker.sock/events');
+        eventSource.addEventListener('message', () => {
+            retrieveInfo();
+        });
+
+        // Cleanup on unmount
+        return () => {
+            eventSource.close();
+        };
+    }, []);
+
+    return (<div>
+        <h1>Docker Plugin Interface (React)</h1>
+        <div id="status">{status}</div>
+        <button onClick={installDocker}>Install Docker</button>
+        <button onClick={buildDocker}>Build Docker Image</button>
+        <button onClick={dockerCompose}>Docker Compose</button>
+        <Terminal logs={logs}/>
+        <div className="container-fluid">
+            <h2>Docker Daemon Info</h2>
+            <ul>
+                <li>Total Memory: <span>{dockerInfo.MemTotal}</span></li>
+                <li>Go Routines: <span>{dockerInfo.NGoroutines}</span></li>
+                <li>File Descriptors: <span>{dockerInfo.NFd}</span></li>
+                <li>Containers: <span>{dockerInfo.Containers}</span></li>
+                <li>Images: <span>{dockerInfo.Images}</span></li>
+            </ul>
+        </div>
+    </div>)
+
+};
+export default DockerService;
--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,9 @@
-/*
- * This file is part of Cockpit.
- *
- * Copyright (C) 2017 Red Hat, Inc.
- *
- * Cockpit is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * Cockpit is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
- */
-
 import cockpit from 'cockpit';
 import React from 'react';
 import { Alert } from "@patternfly/react-core/dist/esm/components/Alert/index.js";
 import { Card, CardBody, CardTitle } from "@patternfly/react-core/dist/esm/components/Card/index.js";
+import DockerService from "./DockerService.jsx";
+
 
 const _ = cockpit.gettext;
 
@@ -43,6 +26,7 @@ export class Application extends React.Component {
                         variant="info"
                         title={ cockpit.format(_("Running on $0"), this.state.hostname) }
                     />
+                    <DockerService />
                 </CardBody>
             </Card>
         );
